81c7805c9643fab4576b8fd8bce8b9c6ae8b0e6b
Vasudev suthar <doyalvasudev87@gmail.com>
2024-02-10 00:16:51 -0800

    add more controllers

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -3,12 +3,12 @@ import { ApiError } from "../utils/ApiError.js"
 import { Product } from "../models/product.model.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
-import mongoose from "mongoose"
+import mongoose, {isValidObjectId} from "mongoose"
 
 
 const addProduct = asyncHandler(async (req, res) => {
 
-    const {CategoryName, ProductName, description} = req.body
+    const { CategoryName, ProductName, description } = req.body
     // console.log("email: ", email)
 
     if (
@@ -44,6 +44,112 @@ const addProduct = asyncHandler(async (req, res) => {
 
 })
 
+const getProduct = asyncHandler(async (req, res) => {
+    const products = await Product.find()
+
+    if (!products) {
+        throw new ApiError(400, "products are not found")
+    }
+
+    else if (products.length > 0) {
+        res.status(201).json(
+            new ApiResponse(200, products, "products fetched successfully")
+        )
+    }
+
+    else {
+        res.status(201).json(
+            new ApiResponse(200, "currantly have not any products")
+        )
+    }
+})
+
+const updateProductDetails = asyncHandler(async (req, res) => {
+
+    const { productId } = req.params
+    const { CategoryName, ProductName, description } = req.body;
+
+    if (!isValidObjectId(productId)) {
+        throw new ApiError(400, "Invalid productId");
+    }
+
+    if (!(CategoryName && ProductName && description)) {
+        throw new ApiError(400, "CategoryName, ProductName and description are required");
+    }
+
+    const product = await Product.findById(productId);
+
+    if (!product) {
+        throw new ApiError(404, "No product found");
+    }
+    
+
+    const productImgLocalPath = req.file?.path;
+
+    if (!productImgLocalPath) {
+        throw new ApiError(400, "product image is required");
+    }
+
+    const img = await uploadOnCloudinary(productImgLocalPath);
+
+    if (!img) {
+        throw new ApiError(400, "product image not found");
+    }
 
 
-export{addProduct}
+    const updateProduct = await Product.findByIdAndUpdate(
+        productId,
+        {
+            $set: {
+                CategoryName,
+                ProductName,
+                description,
+                img: img.url
+            }
+        },
+        { new: true }
+    );
+
+    if (!updateProduct) {
+        throw new ApiError(500, "Failed to update product please try again");
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, updateProduct, "Product updated successfully"));
+})
+
+const deleteProduct = asyncHandler(async (req, res) => {
+
+    const { productId } = req.params
+
+    if (!isValidObjectId(productId)) {
+        throw new ApiError(400, "Invalid productId");
+    }
+
+    const product = await Product.findById(productId);
+
+    if (!product) {
+        throw new ApiError(404, "No product found");
+    }
+    
+
+    const deleteProduct = await Product.findByIdAndDelete(productId);
+
+    if (!deleteProduct) {
+        throw new ApiError(500, "Failed to delete product please try again");
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, deleteProduct, "Product deleted successfully"));
+})
+
+
+
+export {
+    addProduct,
+    getProduct,
+    updateProductDetails,
+    deleteProduct
+}
--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {addProduct} from "../controllers/product.controller.js";
+import {addProduct, deleteProduct, getProduct, updateProductDetails} from "../controllers/product.controller.js";
 import { upload } from "../middlewares/multer.middleware.js"
 
 const router = Router()
@@ -13,5 +13,10 @@ router.route("/addproduct").post(
     ]),
     addProduct
     )
+
+router.route("/getproduct").get(getProduct)
+router.route("/updateproduct/:productId").put(upload.single("img"),updateProductDetails)
+router.route("/deleteproduct/:productId").delete(deleteProduct)
+
 
 export default router
